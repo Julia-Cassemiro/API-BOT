afc36cd9b944b07d3852b05071e0dd740a054a8a
Julia Gomes Cassemiro <julia.cassemiro@vaxxinova.com.br>
2023-01-12 03:10:03 -0300

    Adicionar arquivos de projeto.

---
+++ API-Clinica/Controllers/BotController.cs
@@ -0,0 +1,83 @@
+﻿using API_Clinica.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API_Clinica.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BotController : ControllerBase
+    {
+        // POST api/<BotController>
+        [HttpPost]
+        public object Post(Credentials_Request bot)
+        {
+            var Id = bot.Id;
+            var Text = bot.Text;
+            var Data = bot.Data;
+
+            
+            var Contact = bot.Contact;
+            var uid = Contact.Uid;
+            var type = Contact.Type;
+            var Key = Contact.Key;
+            var Name = Contact.Name;
+            
+
+            var Fields = Contact.Fields;
+            var CPF = Fields.Cpf;
+            var celular = Fields.Celular;
+
+            // VOCES VAO COLOCAR OS TRATAMENTOS
+            //SQL RECEBE ALGUMA VARIAVEL
+            //FAZ UM WHERE COM A VARIAVEL
+            //O QUE RETORNAR DESSE WHERE
+            //VCS PODEM COLOCAR NA RESPONSE.
+
+            var response = new Credentials_Response
+            {
+                Attachments = new Attachments
+                {
+                    Position = "AFTER",  //copiei pdf
+                    Type = "D O C U M E N T",
+                    Name = Name,
+                    Url = "https/teste.com.br"
+
+                },
+                Type = "INFORMATION",
+                Text = Text + "teste aonde vc pode escrever o que quiser",
+
+            };
+
+            var teste = response;
+            return new JsonResult(teste);
+        }
+
+
+
+        //retorno json assim oh
+        //[HttpPost("{Json}")]
+        //public object PostReturnJson()
+        //{
+        //    var payload = new Credentials_Request
+        //    {
+        //        Contact = new Contact
+        //        {
+        //            Name = "Agent Name",
+        //        },
+        //        Id = 23
+        //    };
+
+        //    var teste = payload;
+        //    return new JsonResult(teste);
+
+        //}
+
+
+    }
+}
---
+++ API-Clinica/Model/BotResponse.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+
+
+
+namespace API_Clinica.Model
+{
+    public class Credentials_Response
+    {
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+        [JsonProperty("text")]
+
+        public string Text { get; set; }
+        public Attachments Attachments { get; set; }
+
+
+    }
+
+    public class Attachments
+    {
+        [JsonProperty("position")]
+        public string Position { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("url")]
+        public string Url { get; set; }
+    }
+}
